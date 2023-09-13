2cadc318908f1b6ed16be1c4b1f996e1c0c5f8bc
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-09-13 16:46:38 +0700

    install laravel-kafka

---
+++ app/Infrastructure/Kafka/Producer.php
@@ -0,0 +1,15 @@
+<?php
+
+use Junges\Kafka\Facades\Kafka;
+use Junges\Kafka\Producers\MessageBatch;
+use Junges\Kafka\Message\Message;
+
+class Producer
+{
+    public static function produce($topic, $message)
+    {
+        // $producer = Kafka::publishOn($topic)->withBodyKey('key', $message);
+        // $producer->send();
+        return "it's working";
+    }
+}
