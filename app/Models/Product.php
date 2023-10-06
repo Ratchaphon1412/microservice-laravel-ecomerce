3b43ceb5f64643026b4fbb50c5bd95e81aa5bd7f
saran <saran@MacBook-Air-khxng-saran.local>
2023-10-06 16:23:26 +0700

    create : imageProduct model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Product extends Model
 {
@@ -13,5 +14,10 @@ class Product extends Model
 
     public function colors():BelongsToMany{
         return $this->belongsToMany(Color::class, 'product_color','product_id','color_id');
+    }
+
+    public function images() : HasMany
+    {
+        return $this-> hasMany(ImageProduct ::class);
     }
 }
