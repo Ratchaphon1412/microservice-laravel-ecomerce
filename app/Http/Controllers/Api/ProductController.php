021c556209bd73e181d99bf25596673cdb1645b5
eviperz <jirapa.wong@ku.th>
2023-10-31 22:13:24 +0700

    Add Api/ProductController

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Color;
+use App\Models\Product;
+use App\Models\ProductColor;
+use App\Models\Stock;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index() {
+        return Product::get();
+    }
+    public function store(Request $request) {
+        $request->validate([
+            'name' => 'required|string|max:30',
+            'description' => 'required|string',
+            'price' => 'required|numeric',
+            'category_type' => 'required|string|max:255',
+            'gender' => 'required|string|max:255',
+        ]);
+
+        $product = new Product();
+        $product->name = $request->get('name');
+        $product->description = $request->get('description');
+        $product->price = $request->get('price');
+        $product->category_type = $request->get('category_type');
+        $product->gender = $request->get('gender');
+        $product->save();
+        $product->refresh();
+
+        return $product;
+    }
+    public function show(Product $product) {
+        return Product::with('product_colors')->find($product->id);
+    }
+    public function destroy(Product $product) {
+        $product->delete();
+        return ['success' => 'delete this Product'];
+    }
+
+    // Add Color
+    public function addColor(Request $request, Product $product) {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'hex_color' => 'required|string|max:7',
+        ]);
+        
+        $color = new Color();
+        $color->name = $request->get('name');
+        $color->hex_color = $request->get('hex_color');
+        $color->save();
+        $color->refresh();
+
+        $product_color = new ProductColor();
+        $product_color->product_id = $product->id;
+        $product_color->color_id = $color->id;
+        $product_color->save();
+        $product_color->refresh();
+
+        return Product::with('product_colors')->find($product->id);
+    }
+
+    public function addStock(Request $request, ProductColor $product_color) {
+        $request->validate([
+            'size' => 'required|in:XXS,XS,S,M,L,XL,2XL,3XL',
+            'quantity' => 'required|numeric',
+        ]);
+
+        if (!Stock::where('product_color_id', $product_color->id)
+            ->where('size', $request->get('size'))
+            ->exists()) {
+            $stock = new Stock();
+            $stock->product_color_id = $product_color->id;
+            $stock->size = $request->get('size');
+            $stock->quantity = $request->get('quantity');
+            $stock->save();
+            $stock->refresh();
+        } else {
+            $stock = Stock::where('product_color_id', $product_color->id)
+            ->where('size', $request->get('size'))->first();
+            $stock->quantity += $request->get('quantity');
+            $stock->save();
+            $stock->refresh();
+        }
+
+        return $stock;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Models\Product;
+use App\Http\Controllers\Api\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,11 +14,10 @@ use App\Models\Product;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::resource('/product',ProductController::class);
+Route::apiResource('/product', ProductController::class);
+Route::post('/product/add-color/{product}', [ProductController::class, 'addColor']);
+Route::post('/product/add-stock/{product_color}', [ProductController::class, 'addColor']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
-Route::resource('test', \App\Http\Controllers\TestController::class);
